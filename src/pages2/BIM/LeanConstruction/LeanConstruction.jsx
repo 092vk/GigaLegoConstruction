c6cfa4e0f2fea7ebdcebe04fed48b28477760ef1
Apiksha <panditaapiksha@gmail.com>
2024-07-13 18:19:31 +0530

    Added pages in pages2

---
+++ src/pages2/BIM/LeanConstruction/LeanConstruction.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './LeanConstruction.css'
+
+
+function LeanConstruction() {
+  return (
+    <div className='LeanConstruction'>
+
+        <h1 className='head1'>This is Lean Construction page </h1>
+        <p className='p1'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni expedita in, unde nam reprehenderit quod obcaecati error veritatis placeat, molestias nobis deleniti est labore voluptas ex molestiae consequuntur. Quae excepturi nisi enim, recusandae eaque doloremque, aliquam iste libero neque quibusdam ad? Ipsa numquam qui cumque officia, facere quas soluta iste libero aliquid veritatis esse aut illo accusamus suscipit repellendus, ut sapiente deleniti voluptatibus tenetur sint eligendi non tempore dolorum! Similique, officia nesciunt pariatur aliquid labore doloribus assumenda consectetur blanditiis, error qui nam vitae voluptates sequi quasi dolor fugit modi id debitis et magni, explicabo at nihil aperiam repudiandae! Est, illum.</p>
+
+
+    </div>
+  )
+}
+
+export default LeanConstruction
---
+++ src/pages2/Civil/DrainageSewerageDesign/DrainageSewerageDesign.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './DrainageSewerageDesign.css'
+
+
+function DrainageSewerageDesign() {
+  return (
+    <div className='DrainageSewerageDesign'>
+
+        <h1 className='head1'>This is Drainage Sewerage Design page </h1>
+        <p className='p1'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni expedita in, unde nam reprehenderit quod obcaecati error veritatis placeat, molestias nobis deleniti est labore voluptas ex molestiae consequuntur. Quae excepturi nisi enim, recusandae eaque doloremque, aliquam iste libero neque quibusdam ad? Ipsa numquam qui cumque officia, facere quas soluta iste libero aliquid veritatis esse aut illo accusamus suscipit repellendus, ut sapiente deleniti voluptatibus tenetur sint eligendi non tempore dolorum! Similique, officia nesciunt pariatur aliquid labore doloribus assumenda consectetur blanditiis, error qui nam vitae voluptates sequi quasi dolor fugit modi id debitis et magni, explicabo at nihil aperiam repudiandae! Est, illum.</p>
+
+
+    </div>
+  )
+}
+
+export default DrainageSewerageDesign
