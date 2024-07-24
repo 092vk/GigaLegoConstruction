a2944a973b9af9d5d6e6d3884a60d149074b8794
092vk <vk092kumar@gmail.com>
2024-07-24 12:01:19 +0530

    a

--- src/utils/dataSliderImage/ApData.js
+++ src/utils/dataSliderImage/ApData.js
@@ -1,17 +1,17 @@
 const imageSlide =[
     {
         'Name':'abc',
-        'url':'../../../public/r1.png',
+        'url':'./r1.png',
         'des':'hhh'
     },
     {
         'Name':'cde',
-        'url':'../../../public/r2.png',
+        'url':'./r2.png',
         'des':'sdksdsdskdsncc sdmnsdm'
     },
     {
         'Name':'fdg',
-        'url':'../../../public/r3.png',
+        'url':'./r3.png',
         'des':'tu ta ra , ram pam pam '
     }
 ]
